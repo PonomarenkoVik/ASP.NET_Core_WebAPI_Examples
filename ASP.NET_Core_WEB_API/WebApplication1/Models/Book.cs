317345a0c4450676d33e1fcb1755240836bb7c23
v.ponomarenko <Ponomarenkovic1979@gmail.com>
2021-03-07 20:06:49 +0200

    Add WebApplication1

---
+++ ASP.NET_Core_WEB_API/WebApplication1/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Pages { get; set; }
+
+        [Required]
+        public ushort Year { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public int PublisherId { get; set; }
+    }
+}
---
+++ ASP.NET_Core_WEB_API/WebApplication1/Models/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+    }
+}
